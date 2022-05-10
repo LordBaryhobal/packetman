38c742285ffab4833084a9af8be528d5cfff677e
Lord Baryhobal <lordbaryhobal@gmail.com>
2022-05-10 21:25:16 +0200

    refactored Vec, added copyright disclaimer

--- classes/Tile.py
+++ classes/Tile.py
@@ -1,12 +1,18 @@
+#Packetman is a small game created in the scope of a school project
+#Copyright (C) 2022  Louis HEREDERO & Mathéo BENEY
+
+from typing import List, Type
 import pygame
+from Vec import Vec
 
 class Tile:
-    def __init__(self, x, y, size, color,type):
+    def __init__(self, x: int, y: int, size, color: List[int, int, int], type_: int) -> None:
         self.x = x
         self.y = y
         self.size = size
         self.color = color
-        self.type = type # 0 = empty, 1 = ..., 2 = ..., 3 = ...
-    def render(self, surface, position):
+        self.type = type_ # 0 = empty, 1 = ..., 2 = ..., 3 = ...
+    
+    def render(self, surface: Type[pygame.Surface], position: Type[Vec]) -> None:
         pygame.draw.rect(surface, self.color, (position.x, position.y, self.size, self.size))
     
--- classes/Vec.py
+++ classes/Vec.py
@@ -1,34 +1,59 @@
+#Packetman is a small game created in the scope of a school project
+#Copyright (C) 2022  Louis HEREDERO & Mathéo BENEY
+
 import math
 
-class Vector() :
-    def __init__(self, x, y) :
+class Vec() :
+    """
+    Simple vector class
+    """
+
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
-    def __add__(self, other) :
-        return Vector(self.x + other.x, self.y + other.y)
-    def __sub__(self, other) :
-        return Vector(self.x - other.x, self.y - other.y)
-    def __mul__(self, other) :
-        return Vector(self.x * other, self.y * other)
-    def __truediv__(self, other) :
-        return Vector(self.x / other, self.y / other)
-    def __floordiv__(self, other) :
-        return Vector(self.x // other, self.y // other)
-    def __mod__(self, other) :
-        return Vector(self.x % other, self.y % other)
-    def __pow__(self, other) :
-        return Vector(self.x ** other, self.y ** other)
-    def __neg__(self) :
-        return Vector(-self.x, -self.y)
-    def dot(self, other) :
+    
+    def __add__(self, other):
+        return Vec(self.x + other.x, self.y + other.y)
+    
+    def __sub__(self, other):
+        return Vec(self.x - other.x, self.y - other.y)
+    
+    def __mul__(self, other):
+        return Vec(self.x * other, self.y * other)
+    
+    def __truediv__(self, other):
+        return Vec(self.x / other, self.y / other)
+    
+    def __floordiv__(self, other):
+        return Vec(self.x // other, self.y // other)
+    
+    def __mod__(self, other):
+        return Vec(self.x % other, self.y % other)
+    
+    def __pow__(self, other):
+        return Vec(self.x ** other, self.y ** other)
+    
+    def __neg__(self):
+        return Vec(-self.x, -self.y)
+    
+    def dot(self, other):
         return self.x * other.x + self.y * other.y
-    def length(self) :
+    
+    @property
+    def length(self):
         return (self.x ** 2 + self.y ** 2) ** 0.5
-    def normalize(self) :
-        return self / self.length()
-    def get_angle(self) :
+    
+    def normalize(self):
+        l = self.length
+        if l == 0:
+            return Vec()
+        return self / l
+    
+    def get_angle(self):
         return math.atan2(self.y, self.x)
-    def rotate(self, angle) :
-        return Vector(self.x * math.cos(angle) - self.y * math.sin(angle), self.x * math.sin(angle) + self.y * math.cos(angle))
-    def __abs__(self) :
-        return Vector(abs(self.x), abs(self.y))
+    
+    def rotate(self, angle):
+        return Vec(self.x * math.cos(angle) - self.y * math.sin(angle), self.x * math.sin(angle) + self.y * math.cos(angle))
+    
+    def __abs__(self):
+        return Vec(abs(self.x), abs(self.y))
