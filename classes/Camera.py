5513768e944a50de64084cee90163b999907439e
Lord Baryhobal <lordbaryhobal@gmail.com>
2022-05-11 21:52:18 +0200

    added Animation, Camera and Event classes

---
+++ classes/Camera.py
@@ -0,0 +1,27 @@
+#Packetman is a small game created in the scope of a school project
+#Copyright (C) 2022  Louis HEREDERO & Mathéo BENEY
+
+class Camera:
+    """
+    Camera class responsible for calculating the position and sizes
+    of elements to render on the screen. Calls the render functions
+    of tiles and entities.
+    """
+
+    def __init__(self, game):
+        self.game = game
+
+    def render(self, surface):
+        pass
+
+    def screen_to_world(self, pos):
+        """Converts screen to world coordinates
+        @param pos: Vec to convert
+        """
+        pass
+
+    def world_to_screen(self, pos):
+        """Converts world to screen coordinates
+        @param pos: Vec to convert
+        """
+        pass
