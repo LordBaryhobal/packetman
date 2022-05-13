5b6a2d337f5ce18433b549f7eb412ab8db8809fc
MatBB3 <you@example.com>
2022-05-14 00:34:27 +0200

    added basic camera rendering

--- classes/Camera.py
+++ classes/Camera.py
@@ -1,5 +1,12 @@
 #Packetman is a small game created in the scope of a school project
 #Copyright (C) 2022  Louis HEREDERO & Mathéo BENEY
+from .Vec import Vec
+
+#TODO: fix circular import
+try:
+    from .Game import Game
+except ImportError:
+    pass
 
 class Camera:
     """
@@ -9,19 +16,31 @@ class Camera:
     """
 
     def __init__(self):
-        pass
+        #camera's coordinates are in the bottom left corner of the screen
+        self.coo = Vec()
+        self.game = Game.instance
+        self.tilesize = self.game.HEIGHT//10
+        
+    def uptade_visible_tiles(self):
+        bottomright = self.screen_to_world(self.coo+Vec(0,self.coo.y))
+        topleft = self.screen_to_world(self.coo+Vec(self.game.WIDTH,0))
+        self.visible_tiles = self.game.world.get_tiles_in_rect(topleft, bottomright).flatten()
 
     def render(self, surface):
-        pass
+        """Renders the visible tiles and entities
+        @param surface: pygame surface to render on
+        """
+        for tile in self.visible_tiles:
+            tile.render(surface, self.world_to_screen(tile.coo))
 
     def screen_to_world(self, pos):
         """Converts screen to world coordinates
         @param pos: Vec to convert
         """
-        pass
+        return (Vec(self.game.HEIGHT,self.game.WIDTH)-pos+self.coo)//self.tilesize
 
     def world_to_screen(self, pos):
         """Converts world to screen coordinates
         @param pos: Vec to convert
         """
-        pass
+        return Vec(self.game.HEIGHT,self.game.WIDTH)+self.coo-(pos*self.tilesize)
--- classes/Tile.py
+++ classes/Tile.py
@@ -5,13 +5,11 @@ from .Vec import Vec
 import pygame
 
 class Tile:
-    def __init__(self, x, y, size, color, type_):
-        self.x = x
-        self.y = y
-        self.size = size
-        self.color = color
+    def __init__(self, x, y, type_):
+        self.coo = Vec(x, y)
+        self.color = [(255,255,255),(0,0,0),(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255,255)]
         self.type = type_ # 0 = empty, 1 = ..., 2 = ..., 3 = ...
     
-    def render(self, surface, position):
-        pygame.draw.rect(surface, self.color, (position.x, position.y, self.size, self.size))
+    def render(self, surface, position,size):
+        pygame.draw.rect(surface, self.color, (position.x, position.y, size, size))
     
