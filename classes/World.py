bac1582f03120bb3912f38f7dcb9ec31ed7bd0b5
Lord Baryhobal <lordbaryhobal@gmail.com>
2022-05-15 01:34:26 +0200

    added simple collision btw entities / solid tiles

--- classes/World.py
+++ classes/World.py
@@ -20,19 +20,19 @@ class World:
         self.create_tilelist()
         self.entities = []
 
-        self.entities.append(Entity(Vec(1.5,5)))
+        self.entities.append(Entity(Vec(1.5,5), vel=Vec(1)))
         
     def create_tilelist(self):
         #arr = np.random.randint(0,8,(self.HEIGHT,self.WIDTH))
         arr = np.array([
             [1,2,1,2,1,2,1,2],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0]
+            [3,0,0,0,0,0,0,0],
+            [4,0,0,0,0,0,0,0],
+            [3,0,0,0,0,0,0,0],
+            [4,0,0,0,0,0,0,0],
+            [3,0,0,0,0,0,0,0],
+            [4,0,0,0,0,0,0,0],
+            [3,0,0,0,0,0,0,0]
         ])
 
         self.tiles = np.empty([self.HEIGHT,self.WIDTH], dtype='object')
@@ -43,6 +43,8 @@ class World:
     def physics(self, delta):
         for entity in self.entities:
             entity.physics(delta)
+
+            self.check_collisions(entity, delta)
     
     def get_tiles_in_rect(self, topleft, bottomright):
         return self.tiles[bottomright.y:topleft.y+1, topleft.x:bottomright.x+1]
@@ -51,3 +53,42 @@ class World:
         rect = Rect(topleft.x, bottomright.y, bottomright.x-topleft.x, topleft.y-bottomright.y)
 
         return list(filter(lambda e: e.box.overlaps(rect), self.entities))
+    
+    def check_collisions(self, entity, delta):
+        v = entity.vel.length
+
+        tl = Vec( int(entity.pos.x), int(entity.pos.y+entity.box.h))
+        br = Vec( int(entity.pos.x+entity.box.w), int(entity.pos.y))
+
+        tiles = self.get_tiles_in_rect(tl,br).flatten()
+
+        for tile in tiles:
+            if tile.type != 0 and tile.solid:
+                print(tile.coo)
+                dx, dy = 0, 0
+                if entity.vel.x < 0:
+                    dx = tile.coo.x - entity.pos.x
+                elif entity.vel.x > 0:
+                    dx = entity.pos.x+entity.box.w - tile.coo.x
+                
+                if entity.vel.y < 0:
+                    dy = tile.coo.y - entity.pos.y + entity.box.h
+                elif entity.vel.y > 0:
+                    dy = entity.pos.y - tile.coo.y + 1
+
+                d1 = -v * dx / entity.vel.x if entity.vel.x != 0 else 0
+                d2 = -v * dy / entity.vel.y if entity.vel.y != 0 else 0
+                print(d1, d2)
+                print(entity.vel)
+
+                d = min(d1, d2) if d1*d2 != 0 else max(d1, d2)
+                
+                entity.pos -= entity.vel.normalize()*d
+                
+                if d != 0:
+                    if d1 < d2:
+                        entity.vel.x = 0
+                    else:
+                        entity.vel.y = 0
+                input()
+        
