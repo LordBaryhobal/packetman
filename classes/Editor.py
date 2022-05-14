29b492e7371045a878be3c5dc20d59f34e4e913d
MatBB3 <you@example.com>
2022-05-14 18:55:27 +0200

    added a simple selection mechanism

--- classes/Editor.py
+++ classes/Editor.py
@@ -8,24 +8,33 @@ class Editor():
     
     def __init__(self,game):
        self.game = game
+       self.current_type = 0
        self.startmove = None
        self.moving = False
        self.placing = False
-       self.current_type = 0
+       self.selecting = False
+       self.selection = None
     
        
     
     def handle_events(self,events):
+        
+        self.game.logger.debug(str(self.selection))
         
         if self.moving:
             newpos = Vec(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1])
             self.game.camera.coo += (self.startmove-newpos)*Vec(1,-1)
             self.startmove = newpos
             self.game.camera.update_visible_tiles()
+            self.selection = None
             
-        if self.placing:
+        if self.placing and self.selection is None:
             self.game.world.set_tile(self.game.camera.screen_to_world(Vec(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1])),self.current_type)
             self.game.camera.update_visible_tiles()
+        
+        if self.selecting:
+            #self.drawselectionbox(self.selection[0],self.game.camera.screen_to_world(Vec(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1])))
+            pass
         
         for event in events:
             
@@ -37,18 +46,44 @@ class Editor():
                 if event.button == 2:
                     self.startmove = Vec(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1])
                     self.moving = True
+
+                if event.button == 3:
+                    self.selection = [self.game.camera.screen_to_world(Vec(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1]))]
+                    self.selecting = True
             
             elif event.type == pygame.MOUSEBUTTONUP:
                 
                 if event.button == 1:
                     self.placing = False
                     
-                if event.button == 2:
+                elif event.button == 2:
                     self.moving = False
+                
+                elif event.button == 3:
+                    self.selection.append(self.game.camera.screen_to_world(Vec(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1])))
+                    if self.selection[0] == self.selection[1]:
+                        self.selection = None
+                    else:
+                        v1 = self.selection[0].min(self.selection[1])
+                        v2 = self.selection[0].max(self.selection[1])
+                        self.selection = [v1,v2]
+                    self.selecting = False
             
             elif event.type == pygame.KEYDOWN:
                 if event.key in (pygame.K_0,pygame.K_1,pygame.K_2,pygame.K_3,pygame.K_4,pygame.K_5,pygame.K_6,pygame.K_7,pygame.K_8,pygame.K_9):
                     self.current_type = event.key-48
+                elif event.key == pygame.K_f:
+                    if self.selection is not None:
+                        self.modify_selection(self.current_type)
+                elif event.key == pygame.K_BACKSPACE:
+                    if self.selection is not None:
+                        self.modify_selection(0)
+    
+    def modify_selection(self,type):
+        for x in range(self.selection[0].x,self.selection[1].x+1):
+            for y in range(self.selection[0].y,self.selection[1].y+1):
+                self.game.world.set_tile(Vec(x,y),type)
+        self.game.camera.update_visible_tiles()
             
                     
                  
