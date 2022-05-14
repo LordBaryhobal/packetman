5567efc09611f48b3f158f8ae2ff6e6a0ece4128
LordBaryhobal <47377497+LordBaryhobal@users.noreply.github.com>
2022-05-14 16:32:42 +0200

    Revert "Apply entities-physics on main" (#9)
    * Revert "disabled physics in editor mode"
    
    This reverts commit 7275b31d4d30f1e02979838f6855a7326349aee8.
    
    * Revert "added entity rendering"
    
    This reverts commit f851667838e2dd0a3066383f28f5e141cfe0d340.
    
    * Revert "added Rect"
    
    This reverts commit 6789b7ca5e0616c1b728672dd3347563c406ed72.
    
    * Revert "fixed typo"
    
    This reverts commit e42d441f37d80f8304f518d72372313549801730.

--- classes/World.py
+++ classes/World.py
@@ -3,9 +3,6 @@
 
 import numpy as np
 from .Tile import Tile
-from .Entity import Entity
-from .Vec import Vec
-from .Rect import Rect
 
 class World:
     """
@@ -18,9 +15,6 @@ class World:
 
     def __init__(self):
         self.create_tilelist()
-        self.entities = []
-
-        self.entities.append(Entity(Vec(5,5)))
         
     def create_tilelist(self):
         arr = np.random.randint(0,8,(self.HEIGHT,self.WIDTH))
@@ -31,11 +25,5 @@ class World:
     
     def physics(self, delta):
         pass
-    
     def get_tiles_in_rect(self, topleft, bottomright):
         return self.tiles[bottomright.y:topleft.y+1, topleft.x:bottomright.x+1]
-    
-    def get_entities_in_rect(self, topleft, bottomright):
-        rect = Rect(topleft.x, bottomright.y, bottomright.x-topleft.x, topleft.y-bottomright.y)
-
-        return list(filter(lambda e: e.box.overlaps(rect), self.entities))
