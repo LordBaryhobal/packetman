e0c8150f5f9180317c36de9af9875a068faf025a
Lord Baryhobal <lordbaryhobal@gmail.com>
2022-05-15 23:37:11 +0200

    added config for editor hud

--- classes/Hud.py
+++ classes/Hud.py
@@ -10,6 +10,8 @@ class Hud:
     """Class to display editor hud"""
 
     MARGIN = 20
+    SLOT_COL = (150,150,150)
+    SLOT_INDICATOR_COL = (200,80,80)
 
     def __init__(self, game):
         self.game = game
@@ -26,45 +28,60 @@ class Hud:
         return 0
 
     def render(self, surface):
-        WIDTH, HEIGHT = surface.get_width(), surface.get_height()
-        hotbar_width = 0.5*WIDTH
-        hotbar_height = 0.1*hotbar_width
+        # Hotbar
+        hotbar_pos = self.game.config["gui"]["editor"]["hotbar_pos"]
 
-        ox, oy = WIDTH/2 - hotbar_width/2, HEIGHT-hotbar_height-self.MARGIN
-        gap = (hotbar_width-9*hotbar_height)/10
-        margin = hotbar_height*0.1
+        WIDTH, HEIGHT = surface.get_width(), surface.get_height()
+        hotbar_length = 0.5*WIDTH
+        slot_size = 0.1*hotbar_length
+
+        if hotbar_pos == 0:
+            ox, oy = WIDTH/2 - hotbar_length/2, HEIGHT-slot_size-self.MARGIN
+        else:
+            ox, oy = self.MARGIN, HEIGHT/2 - hotbar_length/2
+        
+        gap = (hotbar_length-9*slot_size)/10
+        margin = slot_size*0.1
 
         for i in range(9):
-            x, y = ox + gap + i*(hotbar_height+gap), oy
-            #color = (200,80,80) if i == self.slot else (150,150,150)
-            color = (150,150,150)
-            pygame.draw.rect(surface, color, [x, y, hotbar_height, hotbar_height])
+            if hotbar_pos == 0:
+                x, y = ox + gap + i*(slot_size+gap), oy
+            else:
+                x, y = ox, oy + gap + i*(slot_size+gap)
+            
+            pygame.draw.rect(surface, self.SLOT_COL, [x, y, slot_size, slot_size])
 
             if self.hotbar < len(self.hotbars) and i < len(self.hotbars[self.hotbar]):
-                self.hotbars[self.hotbar][i].render(surface, Vec(x+margin, y+hotbar_height-margin), hotbar_height*0.8)
+                self.hotbars[self.hotbar][i].render(surface, Vec(x+margin, y+slot_size-margin), slot_size*0.8)
         
-        w, h = hotbar_height*0.75, self.MARGIN/4
-        pygame.draw.rect(surface, (200,80,80), [ox + gap + self.slot*(hotbar_height+gap) + hotbar_height*0.5 - w/2, HEIGHT-self.MARGIN*0.75, w, h])
-
-        # Thumbs
-        world_w = self.game.world.WIDTH*self.game.camera.tilesize
-        world_h = self.game.world.HEIGHT*self.game.camera.tilesize
-
-        pos_x, pos_y = self.game.camera.pos.x, self.game.camera.pos.y
+        if hotbar_pos == 0:
+            w, h = slot_size*0.75, self.MARGIN/4
+            pygame.draw.rect(surface, self.SLOT_INDICATOR_COL, [ox + gap + self.slot*(slot_size+gap) + slot_size*0.5 - w/2, HEIGHT-self.MARGIN*0.75, w, h])
         
-        rx = pos_x/(world_w - self.game.WIDTH)
-        ry = pos_y/(world_h - self.game.HEIGHT)
-        rx, ry = max(0, min(1, rx)), max(0, min(1, ry))
-        
-        x_thumb_w = min(1, self.game.WIDTH/world_w) * self.game.WIDTH
-        y_thumb_h = min(1, self.game.HEIGHT/world_h) * self.game.HEIGHT
+        else:
+            w, h = self.MARGIN/4, slot_size*0.75
+            pygame.draw.rect(surface, self.SLOT_INDICATOR_COL, [self.MARGIN*0.75-w, oy + gap + self.slot*(slot_size+gap) + slot_size*0.5 - h/2, w, h])
 
-        x = rx*(self.game.WIDTH-x_thumb_w)
-        y = (1-ry)*(self.game.HEIGHT-y_thumb_h)
 
-        pygame.draw.rect(surface, (255,255,255), [x, HEIGHT-5, x_thumb_w,5])
-        pygame.draw.rect(surface, (255,255,255), [0, y, 5, y_thumb_h])
+        # Scrollbars
+        if self.game.config["gui"]["editor"]["scrollbars"]:
+            world_w = self.game.world.WIDTH*self.game.camera.tilesize
+            world_h = self.game.world.HEIGHT*self.game.camera.tilesize
 
+            pos_x, pos_y = self.game.camera.pos.x, self.game.camera.pos.y
+            
+            rx = pos_x/(world_w - self.game.WIDTH)
+            ry = pos_y/(world_h - self.game.HEIGHT)
+            rx, ry = max(0, min(1, rx)), max(0, min(1, ry))
+            
+            x_thumb_w = min(1, self.game.WIDTH/world_w) * self.game.WIDTH
+            y_thumb_h = min(1, self.game.HEIGHT/world_h) * self.game.HEIGHT
+
+            x = rx*(self.game.WIDTH-x_thumb_w)
+            y = (1-ry)*(self.game.HEIGHT-y_thumb_h)
+
+            pygame.draw.rect(surface, (255,255,255), [x, HEIGHT-5, x_thumb_w,5])
+            pygame.draw.rect(surface, (255,255,255), [0, y, 5, y_thumb_h])
     
     def set_hotbar(self, i):
         if i < len(self.hotbars):
