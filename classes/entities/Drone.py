76b7c7202bcaca466d36fc5719a3158064ea162c
MatBB3 <you@example.com>
2022-05-27 15:37:21 +0200

    modified drone to be activated or not

--- classes/entities/Drone.py
+++ classes/entities/Drone.py
@@ -5,8 +5,9 @@ import pygame
 from classes.Entity import Entity
 from classes.Vec import Vec
 from math import copysign, floor
+from classes.Event import Event, listener, on
 
-
+@listener
 class Drone(Entity):
     
     _entity = {
@@ -16,14 +17,33 @@ class Drone(Entity):
     
     SPEED = 2
     SIZE = Vec(0.7,0.7)
-    GRAVITY = False
+    GRAVITY = True
+    INTERACTIVE = True
+    
+    def __init__(self, pos=None, vel=None, acc=None, type_=None, highlight=False, world=None):
+        super().__init__(pos, vel, acc, type_, highlight, world)
+        self.following = False
     
     def handle_events(self, events):
-        player = self.world.player
-        player_pos = player.pos + player.SIZE/2
-        
-        current_pos = self.pos + self.SIZE/2
-        
-        direction = (player_pos-current_pos).normalize()
-        self.vel = direction * self.SPEED
+        if self.following:
+            player = self.world.player
+            player_pos = player.pos + player.SIZE/2
+            
+            current_pos = self.pos + self.SIZE/2
+            
+            direction = (player_pos-current_pos).normalize()
+            self.vel = direction * self.SPEED
+    
+    @on(Event.INTERACTION)
+    def on_interract(self, event):
+        if self in event.entities:
+            self.update_following(not self.following)
+            
+    def update_following(self, following):
+        self.following = following
+        if not self.following:
+            self.vel = Vec()
+            self.GRAVITY = True
+        else:
+            self.GRAVITY = False
     
