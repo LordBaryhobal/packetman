79a2ee9be2d4b3e049aaff77b5b6bca160724e74
Lord Baryhobal <lordbaryhobal@gmail.com>
2022-06-05 17:52:14 +0200

    added background + time bar to story texts

--- classes/TextManager.py
+++ classes/TextManager.py
@@ -13,6 +13,8 @@ class TextManager:
 
     DELAY_PER_CHAR = 0.2
     FADE_DURATION = 0.5
+    MARGIN = 10
+    TIME_BAR = 2
 
     def __init__(self, game):
         """Initializes a TextManager instance
@@ -46,6 +48,7 @@ class TextManager:
             a = Animation(TM, "opacity", 0, 255, TM.FADE_DURATION)
             a.origin = "TextManager"
             a.id = "fade_in"
+            TM.delay = 0
         
         else:
             TM.displaying = False
@@ -55,20 +58,49 @@ class TextManager:
         W, H = TM.game.WIDTH, TM.game.HEIGHT
 
         if len(TM.lines) > 0:
+            # Add empty line to leave space
+            lines = TM.lines[0].split("\n")[::-1]
+
             s2 = pygame.Surface([W, H], pygame.SRCALPHA)
 
-            y = H
-
-            # Add empty line to leave space
-            lines = [""]+TM.lines[0].split("\n")[::-1]
+            y = H - TM.MARGIN
+            max_w = 0
             
             for l in lines:
-                txt = TM.font.render(l, True, (255,255,255))
+                txt = TM.font.render(l, True, (255, 255, 255))
                 y -= txt.get_height()
-                s2.blit(txt, [W/2-txt.get_width()/2, y])
-            
+                s2.blit(txt, [W/2 - txt.get_width()/2, y])
+                max_w = max(max_w, txt.get_width())
+
             s2.set_alpha(TM.opacity)
+
+            w = max_w + TM.MARGIN*2
+            h_bg = H - y + TM.MARGIN
+            x = W/2 - w/2
+            
+            # Background
+            pygame.draw.rect(surface, (0, 0, 0, TM.opacity), [
+                x, y - TM.MARGIN, w, h_bg])
+            
+            # Time bar
+            pygame.draw.rect(surface, (150, 150, 150, TM.opacity), [
+                x, H - TM.TIME_BAR, w*TM.delay, TM.TIME_BAR])
+
             surface.blit(s2, [0, 0])
+    
+    @on(Event.WORLD_LOADED)
+    def clear(self, *args, **kwargs):
+        """Clears all currently display texts"""
+
+        self.lines = []
+        self.delay = 0
+        self.opacity = 0
+        self.displaying = False
+
+        Animation.ANIMATIONS = list(filter(
+            lambda a: not hasattr(a, "origin") or a.origin != "TextManager",
+            Animation.ANIMATIONS
+        ))
 
     @on(Event.ANIMATION_FINISH)
     def on_anim_finish(self, event):
